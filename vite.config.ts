9d52e150bb2f0ae27a6c9eb63bb89cc89cb3d8bc
tanuH14 <tanusharmaa2720@gmail.com>
2026-06-01 11:11:17 +0530

    Convert project to standard Vite deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart({
-      target: 'vercel',
-    }),
-    react(),
-  ],
-})
+  plugins: [react(), tsconfigPaths()],
+});
